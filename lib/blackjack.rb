f6532413f1908ab6f2c86cee2cec0051099eeb40
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-14 22:17:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,64 @@
 def welcome
+  puts "Welcome to the Blackjack Table"
   # code #welcome here
 end
 
 def deal_card
+  rand 1..11
   # code #deal_card here
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  
+   # code #display_card_total here
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+  gets.chomp
   # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+   if card_total>21
+   puts "Sorry, you hit #{card_total}. Thanks for playing!"
+ end
+   # code
+   #end_game here
 end
 
 def initial_round
+  first_card = deal_card
+  second_card = deal_card
+  display_card_total = first_card+second_card
+  puts "Your cards add up to #{display_card_total}"
+  return display_card_total
   # code #initial_round here
 end
 
-def hit?
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  if input == "s"
+  elsif input == "h"
+    card_total += deal_card
+  else
+    invalid_command
+  end
+  card_total
   # code hit? here
 end
 
 def invalid_command
-  # code invalid_command here
+  
+  rand 1..11
+puts "Please enter a valid command"
+prompt user # code invalid_command here
 end
 
 #####################################################
@@ -39,6 +66,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+    until (card_total>21)
+    card_total = initial_round 
+     end
+  card_total
 end
-    
+end_game
+end
+  # code runner here
+
