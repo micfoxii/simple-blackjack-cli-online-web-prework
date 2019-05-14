7f10074f7be3c3a9c0b642b9b5a822f52ea79cb8
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-14 22:29:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,11 +67,12 @@ end
 
 def runner
   welcome
-  initial_round
+  card_total = initial_round
     until (card_total>21)
-    card_total = initial_round 
+    card_total = hit?(card_total)
+    display_card_total(card_total)
 end
-end_game
+end_game(card_total)
 end
   # code runner here
 
